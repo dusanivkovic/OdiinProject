8aae917e27c7bf44263dc9491a79899ce74407ab
https://github.com/dusanivkovic <ivdule77@gmail.com>
2022-12-23 08:51:01 +0100

    Add argument to the Function Game

--- RockPaperScissors/index.js
+++ RockPaperScissors/index.js
@@ -6,40 +6,35 @@ let playerSelection = '';
 let computerSelection = ''; 
 let resultPlayer = document.getElementById('player-result');
 let resultComputer = document.getElementById('computer-result');
-let player = document.getElementById('player_choice');
+let player;
 let startGame = document.getElementById('start');
-function game() {
-if (winComputer > winPlayer) {
-    return 'Win Computer';
-}else if (winComputer < winPlayer) {
-    return 'Win Player'
-}else {
-    return 'Equal'
-}
-}
-
-startGame.addEventListener('click', e => { 
-    ;
-});
-
-player.addEventListener('click', event => {
-    playerSelection = event.target.innerText.toLowerCase();
+function game(e) {
+    playerSelection = e.target.innerText.toLowerCase();
     resultPlayer.innerText = `You: ${playerSelection}`;
     computerSelection = getComputerChoice();
-    resultComputer.innerText = `Computer: ${computerSelection}`;
-    console.log(playerSelection, computerSelection)
-    console.log(playRound(playerSelection, computerSelection));
-});
+    resultComputer.innerHTML = `Computer: ${computerSelection}</br> ${playRound(playerSelection, computerSelection).winComputer}`;
+    // console.log(playerSelection, computerSelection)
+    document.getElementById('result-game').innerText = playRound(playerSelection, computerSelection).result;
+    console.log(playRound(playerSelection, computerSelection).winComputer);
+    if (winComputer > winPlayer) {
+        return 'Win Computer';
+    }else if (winComputer < winPlayer) {
+        return 'Win Player'
+    }else {
+        return 'Equal'
+    }
+}
 
-
-
-
-
+// startGame.addEventListener('click', e => { 
+//     ;
+// });
 
 
 let list = document.getElementsByClassName('player-tool');
 [...list].forEach( (item) => {
-    item.addEventListener('click', console.log('HI'))
+    item.addEventListener('click', (event) => {
+        game(event);
+    })
 })
 
 // player.addEventListener('click', event => {resultPlayer.innerText = game()})
