45528e5dd0a33d4501fe3fb490898fae17a2fc5c
https://github.com/dusanivkovic <ivdule77@gmail.com>
2023-02-02 17:21:34 +0100

    Add clear function, percent style and sign for negative number

--- Calculator/script.js
+++ Calculator/script.js
@@ -5,33 +5,39 @@ gridItem.forEach(item => {
     if (calcSymbol[i] != 'display') {
         item.innerText = calcSymbol[i];
     }
-    item.setAttribute('id', calcSymbol[i]);
+    if (calcSymbol[i] != '%') {
+        item.setAttribute('id', calcSymbol[i]);;
+    }else {
+        item.setAttribute('id', 'percent');
+    }
     i++;
 })
 const clearDisplay = document.querySelector('#AC');
 const display = document.querySelector('#display');
+const span = document.createElement('span');
+span.classList.add('numbers')
+const negativeSign = display.querySelector('#show-negative');
 clearDisplay.addEventListener('click', () => {
-    display.innerHTML = '';
-    const span = document.createElement('span');
-    span.classList.add('negative');
-    // let textNode = span.createTextNode("-");
-    // span(textNode);
-    display.appendChild(span);
+    document.querySelector('.numbers').innerText = '';
+    negativeSign.classList.add('negative'); 
 })
 const sign = document.getElementById('+/-');
 sign.addEventListener('click', () => {
-    const negativeSign = display.querySelector('span');
     if (negativeSign.matches('.negative')) {
         negativeSign.classList.remove('negative');
-    } else {
+    }else {
         negativeSign.classList.add('negative');
     }
 })
 const number = document.querySelectorAll('.number');
 number.forEach(item => {
     item.addEventListener('click', () => {
-        display.innerHTML += '<span>' + item.innerHTML + '</span>';
+        span.innerHTML += item.innerHTML;
+        display.appendChild(span);
     })
 })
-
+const percent = document.querySelector('#percent');
+percent.addEventListener('click', () => {
+    display.querySelector('.numbers').innerText = parseFloat(display.querySelector('.numbers').innerText)/100;
+})
 
